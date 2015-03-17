e342f61d18ae307c85e981887ac6bf8d102e9a7c
isaacabraham <isaac.abraham@gmail.com>
2015-03-17 23:15:29 +0000

    Logging

--- src/DigitRecognizer/1-Naive-Baseline-mbrace.fsx
+++ src/DigitRecognizer/1-Naive-Baseline-mbrace.fsx
@@ -41,6 +41,11 @@ let submissionPath = dataFolder + "submission.csv"
 // move csv files to cluster storage
 
 let mbraceDataFolder = cluster.DefaultStoreClient.FileStore.File
+let logInfo message =
+    cloud {
+        let! worker = Cloud.CurrentWorker
+        return! Cloud.Log <| sprintf "%s: %s" worker.Id message
+    }
 
 let cloudTrain =
     match mbraceDataFolder.Enumerate() |> Seq.tryFind(fun file -> file.Path.Contains "train.csv") with
@@ -53,12 +58,15 @@ let cloudTest =
     | None -> mbraceDataFolder.Upload testPath
 
 // Reading the 50,000 known examples in memory.
-let learn =
+
+let buildTrainingSet =
     cloud {
         //TODO: Cache this later
+        do! logInfo "Reading training set..."
         let! lines = 
             cloudTrain.Path
             |> CloudFile.ReadAllLines
+        do! logInfo <| sprintf "Read (%d) lines" lines.Length
 
         let training =
             lines
@@ -67,59 +75,59 @@ let learn =
             |> Array.map (fun line ->
                 { Label = line.[0]; Image = line.[1..] })
 
-        // Euclidean distance between 2 images.
-        let size = 28 * 28
-        let distance (img1:Image) (img2:Image) =
-            let rec compute acc i =
-                if i = size then acc
-                else
-                    let d = (img1.[i] - img2.[i]) * (img1.[i] - img2.[i])
-                    compute (acc + d) (i + 1)
-            compute 0 0
-
-        // Given an image, find the label of the closest
-        // image from the 50,000 known training images.
-        let classifier img =
-            training
-            |> Seq.minBy (fun example -> distance example.Image img)
-            |> fun closest -> closest.Label
-
-        return classifier }
-
-// Read the 20,000 benchmark images we are trying
-// to predict.
-let testing =
-    cloud {
-        let! lines = cloudTest.Path |> CloudFile.ReadAllLines
-        return            
-            lines.[1..]
-            |> Array.map (fun line -> line.Split ',' |> Array.map int)
-            |> Array.mapi (fun i image -> 
-                { ImageId = i + 1; Image = image } )
+        do! logInfo "Built training set."
+        return training
     }
+
+let predict (trainingSet:Example array) =
+    // Euclidean distance between 2 images.
+    let size = 28 * 28
+    let distance (img1:Image) (img2:Image) =
+        let rec compute acc i =
+            if i = size then acc
+            else
+                let d = (img1.[i] - img2.[i]) * (img1.[i] - img2.[i])
+                compute (acc + d) (i + 1)
+        compute 0 0
+
+    // Given an image, find the label of the closest
+    // image from the 50,000 known training images.
+    fun img ->
+        trainingSet
+        |> Seq.minBy (fun example -> distance example.Image img)
+        |> fun closest -> closest.Label
 
 // Create a submission file:
 // for each of the 20,000 images, produce
 // a predicted label, and save the ImageId
 // and prediction into a file.
+#time
+
 let createSubmission =
     cloud {
+        do! logInfo "Reading test set..."
         let! lines = cloudTest.Path |> CloudFile.ReadAllLines
+        do! logInfo "Read test set."
         let cloudImages =
             lines.[1..]
             |> Array.map (fun line -> line.Split ',' |> Array.map int)
             |> Array.mapi (fun i image -> 
                 { ImageId = i + 1; Image = image } )
-        
+        do! logInfo "Built unknown images."
         return!
             cloudImages.[ .. 10]
             |> Array.map (fun test -> 
                 cloud { 
-                    let! predict = learn
-                    return test.ImageId, (predict test.Image) })
+                    do! logInfo <| sprintf "Predicting ImageId %d" test.ImageId
+                    let! trainingSet = buildTrainingSet
+                    let prediction = predict trainingSet test.Image
+                    do! logInfo <| sprintf "Predicted %d" prediction
+                    return test.ImageId, prediction })
             |> Cloud.Parallel
-    } |> cluster.Run
+    } |> cluster.CreateProcess
 
+createSubmission.GetLogs()
+cluster.GetProcess "4d5c065f51b94f3c9ac47fdba7fcb59d" |> fun x -> x.ShowLogs()
 //    [|
 //        yield "ImageId,Label"
 //        yield! predictions
